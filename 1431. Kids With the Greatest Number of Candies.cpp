34b1af65a799bf2f3b856d2926bb33bb7d3f063f
Abin <bintanghidayatullah021@gmail.com>
2025-02-17 21:42:17 +0700

    updating the algorithm

--- 1431. Kids With the Greatest Number of Candies.cpp
+++ 1431. Kids With the Greatest Number of Candies.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
     vector<bool> result;
-    int yes = 0;
+    int maxval = *max_element(candies.begin(), candies.end());;
     
     for (int i = 0; i < candies.size(); i++) {
-        for (int j = 0; j < candies.size(); j++) {
-            if (candies.at(i) + extraCandies >= candies.at(j)) {
-                yes++;
-            }
-            if (yes == candies.size()) {
-                result.push_back(true);
-                yes = 0;
-            }
-            else if (j == candies.size() - 1 && yes != candies.size()) {
-                result.push_back(false);
-                yes = 0;
-            }
+        if (candies[i] + extraCandies >= maxval) {
+            result.push_back(true);
+        }
+        else {
+            result.push_back(false);
         }
     } 
     return result;
