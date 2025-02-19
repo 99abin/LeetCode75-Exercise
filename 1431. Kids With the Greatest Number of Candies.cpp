d007c105dddecdd37cf98be38cb61442f34ff254
Abin <bintanghidayatullah021@gmail.com>
2025-02-19 10:24:08 +0700

    Added comments

--- 1431. Kids With the Greatest Number of Candies.cpp
+++ 1431. Kids With the Greatest Number of Candies.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-    vector<bool> result;
-    int maxval = *max_element(candies.begin(), candies.end());;
+    vector<bool> result; //variable for the result
+    int maxval = *max_element(candies.begin(), candies.end()); //find the most candies in the candies variable
     
-    for (int i = 0; i < candies.size(); i++) {
-        if (candies[i] + extraCandies >= maxval) {
-            result.push_back(true);
+    for (int i = 0; i < candies.size(); i++) { //Loop to check elements one by one
+        if (candies[i] + extraCandies >= maxval) { //If candies with index i are summed with extra candies are greater than the most candies.. 
+            result.push_back(true); //added "true" to the result variable
         }
-        else {
-            result.push_back(false);
+        else { //if not..
+            result.push_back(false); //added "false" to the result variable
         }
     } 
-    return result;
+    return result; //returns the value in result variable
     }
 };
