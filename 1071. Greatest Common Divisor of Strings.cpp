667a8c6fd173ef8f02b05d60d7a3e3157a0236e7
Abin <bintanghidayatullah021@gmail.com>
2025-02-20 09:05:59 +0700

    added comments

--- 1071. Greatest Common Divisor of Strings.cpp
+++ 1071. Greatest Common Divisor of Strings.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-    if (str1 + str2 != str2 + str1) {
-        return "";
+    if (str1 + str2 != str2 + str1) { //if str1 and str2 do not have the same pattern...
+        return ""; //the result is empty string
     }
     
-    auto gcd = [](int a, int b) -> int {
+    auto gcd = [](int a, int b) -> int { //function to find the gcd of both string lengths
         while (b != 0) {
             int temp = b;
             b = a % b;
-            a = temp;
+            a = temp; 
         }
-        return a;
+        return a; //returns the gcd of both string length
     };
     
-    int gcdLength = gcd(str1.length(), str2.length());
+    int gcdLength = gcd(str1.length(), str2.length()); //variable to store the gcd of both string length
     
-    return str1.substr(0, gcdLength);
+    return str1.substr(0, gcdLength); //returns the result, the result is a substring of str1 that start from index 0 to index of both string length gcd
     }
 };
