73b224dbaa31a9a085635aa1efeaefbfdd1327fc
Abin <bintanghidayatullah021@gmail.com>
2025-02-20 09:22:01 +0700

    added comments

--- 605. Can Place Flowers.cpp
+++ 605. Can Place Flowers.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-    int emptypot = 0;
-    int flower = 0;
-    int count1 = count(flowerbed.begin(), flowerbed.end(), 1);
-    flowerbed.insert(flowerbed.begin(),0);
-    flowerbed.insert(flowerbed.end(),0);
+    int emptypot = 0; //counter variable to store the number of empty pot
+    int flower = 0; //counter variable to store the number of flower that being added 
+    int count1 = count(flowerbed.begin(), flowerbed.end(), 1); //variable to store the number of flower that have been added
+    flowerbed.insert(flowerbed.begin(),0); //adding an empty pot as the first pot
+    flowerbed.insert(flowerbed.end(),0); //adding an empty pot as the last pot
 
-    for (int i = 0; i < flowerbed.size(); i++) {
-        if (flowerbed[i] == 0) {
-                emptypot++;
-        } else {emptypot = 0;};
-        if (emptypot == 3) {
-                flowerbed[i-1] = 1;
-                emptypot = 1;
+    for (int i = 0; i < flowerbed.size(); i++) { //loops to add flowers to available pots
+        if (flowerbed[i] == 0) { //because the flower that being added can't have an adjacent, this statement is created to detect 3 consecutive empty pots
+                emptypot++; //increment if there is an empty pot
+        } else {emptypot = 0;}; //if the pot isn't empty, resert the counter
+        if (emptypot == 3) { //if there are 3 consecutive empty pot...
+                flowerbed[i-1] = 1; //adding flower to the previous pot
+                emptypot = 1; //reset the counter to 1
         }
     }
-    int count2 = count(flowerbed.begin(), flowerbed.end(), 1);
-    return(count2 - count1 >= n);
+    int count2 = count(flowerbed.begin(), flowerbed.end(), 1); //varaible to store the number of flowers
+    return(count2 - count1 >= n); //substracting the number of all flower with the number of flower in the beginning to find the number of flowers newly added
     }
 };
